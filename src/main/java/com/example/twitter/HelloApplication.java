99643598d71f3b10feb68e4a07f71e819273f200
aminghazi <aminghazi1234567@gmail.com>
2022-07-28 17:44:10 +0430

    hmm

--- src/main/java/com/example/twitter/HelloApplication.java
+++ src/main/java/com/example/twitter/HelloApplication.java
@@ -4,16 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import views.LoginPage;
+import views.View;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    public void start(Stage stage) throws Exception {
         stage.setTitle("Hello!");
-        stage.setScene(scene);
+        View.setStage(stage);
+        LoginPage loginPage = new LoginPage();
+        loginPage.start(View.getStage());
+        stage.setTitle("Twitter");
+        stage.setFullScreen(true);
         stage.show();
     }
 
