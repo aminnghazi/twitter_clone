f4fa2021e600f2110d2aadf0dc7b2b17b685b80c
aminghazi <aminghazi1234567@gmail.com>
2022-07-30 01:05:15 +0430

    updating to database

--- src/main/java/models/BusinessUser.java
+++
@@ -1,9 +0,0 @@
-package models;
-
-public class BusinessUser extends User{
-    public BusinessUser(String userName , String passWord ,String securityQuestion,String securityAnswer){
-        super(userName,passWord,securityQuestion,securityAnswer);
-        setIsBusiness(true);
-    }
-
-}
