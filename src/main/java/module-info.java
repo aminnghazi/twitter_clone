d06f68ab1d37a67ee1d5f74a673737ba88b2ec15
aminghazi <aminghazi1234567@gmail.com>
2022-07-30 01:09:31 +0430

    database working

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.twitter {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.twitter to javafx.fxml;
     exports com.example.twitter;
